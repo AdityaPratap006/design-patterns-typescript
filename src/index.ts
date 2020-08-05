1e7fb856ec60ab739f0ee2837fdefff793670e36
Aditya Pratap <adityapratap7344@gmail.com>
2020-08-05 23:00:36 +0530

    code cleanup

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,8 @@ import { Company } from './classes/Company';
 import { CustomMap } from './classes/CustomMap';
 
 const user = new User();
-console.log(user);
-
 const company = new Company();
-console.log(company);
-
 const customMap = new CustomMap('map');
+
 customMap.addMarker(user);
 customMap.addMarker(company);
