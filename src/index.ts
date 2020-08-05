d9c76f204d047f1dc40d952aacd80c50f5d326e7
Aditya Pratap <adityapratap7344@gmail.com>
2020-08-05 19:20:53 +0530

    Using company class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { User } from './classes/User';
+import { Company } from './classes/Company';
 
 const user = new User();
 console.log(user);
+
+const company = new Company();
+console.log(company);
